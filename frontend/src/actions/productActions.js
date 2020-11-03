a4b3386e3ca741a6bde579be860b46d7369582b1
Bernardo-Quina <bernardoquina@gmail.com>
2020-11-03 17:45:53 +0000

    Bringing Redux State Into HomeScreen - useDispatch & useSelector

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -18,7 +18,7 @@ export const listProducts = () => async (dispatch) => {
   } catch (err) {
     dispatch({
       type: PRODUCT_LIST_FAIL,
-      payload: err.response && error.response.data.message
+      payload: err.response && err.response.data.message
         ? err.response.data.message
         : err.message
     })
