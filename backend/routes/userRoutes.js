a67504fa53730142d47f08fd1ffe58c23390766e
Bernardo-Quina <bernardoquina@gmail.com>
2020-11-11 15:42:30 +0000

    Get User By ID & Update User Endpoints (as Admin)

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -7,7 +7,9 @@ import {
   registerUser,
   updateUserProfile,
   getUsers,
-  deleteUser
+  deleteUser,
+  getUserById,
+  updateUser
 } from '../controllers/userController.js'
 
 import { protect, isAdmin } from '../middleware/authMiddleware.js'
@@ -29,5 +31,7 @@ router
 router
   .route('/:id')
     .delete(protect, isAdmin, deleteUser)
+    .get(protect, isAdmin, getUserById)
+    .put(protect, isAdmin, updateUser)
 
 export default router
