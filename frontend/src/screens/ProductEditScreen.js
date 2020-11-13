4a7fb6ff3733ba98198902171a1c5cf85c7d969a
Bernardo-Quina <bernardoquina@gmail.com>
2020-11-13 10:21:09 +0000

    Admin Update Product

--- frontend/src/screens/ProductEditScreen.js
+++ frontend/src/screens/ProductEditScreen.js
@@ -5,7 +5,8 @@ import { useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
 import FormContainer from '../components/FormContainer'
-import { listProductDetails } from '../actions/productActions'
+import { listProductDetails, updateProduct } from '../actions/productActions'
+import { PRODUCT_UPDATE_RESET } from '../constants/productConstants'
 
 const ProductEditScreen = ({ match, history }) => {
   const productId = match.params.id
@@ -23,9 +24,19 @@ const ProductEditScreen = ({ match, history }) => {
   const productDetails = useSelector(state => state.productDetails)
   const { loading, error, product } = productDetails
 
+  const productUpdate = useSelector(state => state.productUpdate)
+  const {
+    loading:loadingUpdate,
+    error:errorUpdate,
+    success:successUpdate
+  } = productUpdate
+
 
   useEffect(() => {
-    
+    if(successUpdate) {
+      dispatch({ type: PRODUCT_UPDATE_RESET })
+      history.push('/admin/productlist')
+    } else {
       if(!product.name || product._id !== productId) {
         dispatch(listProductDetails(productId))
       } else {
@@ -37,13 +48,21 @@ const ProductEditScreen = ({ match, history }) => {
         setCountInStock(product.countInStock)
         setDescription(product.description)
       }
-
-  }, [dispatch, productId, product])
+    }
+  }, [dispatch, productId, product, successUpdate, history])
 
   const submitHandler = e => {
     e.preventDefault()
-    // UPDATE PRODUCT
-    console.log('did it edit? nope');
+    dispatch(updateProduct({
+      _id: productId,
+      name,
+      price,
+      image,
+      brand,
+      category,
+      description,
+      countInStock
+    }))
   }
 
   return (
@@ -53,6 +72,8 @@ const ProductEditScreen = ({ match, history }) => {
       </Link>
       <FormContainer>
         <h1>Edit Product</h1>
+        {loadingUpdate && <Loader />}
+        {errorUpdate && <Message variant='danger'>{errorUpdate}</Message>}
         {loading ? <Loader /> : error ? <Message variant='danger'>{error}</Message> : (
           <Form onSubmit={submitHandler}>
             <Form.Group controlId='name'>
