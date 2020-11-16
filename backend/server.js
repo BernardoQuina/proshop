86e19a3aed30bd9ecccfb608c205557194b87806
Bernardo-Quina <bernardoquina@gmail.com>
2020-11-16 12:20:10 +0000

    Preapare for Deployment

--- backend/server.js
+++ backend/server.js
@@ -25,11 +25,6 @@ if(process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
 }
 
-
-app.get('/', (req, res) => {
-  res.send('API is running...')
-})
-
 // Mount routers
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
@@ -42,6 +37,16 @@ app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_I
 // make uploads a static folder for the browser
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running...')
+  })
+}
 
 
 // Error middleware
